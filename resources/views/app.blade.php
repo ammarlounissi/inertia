6996facf7d34fb708b3638d807822bdc88f15660
ammarlounissi <onismr@gmail.com>
2025-11-30 11:37:20 +0100

    اضافة ملف manifest

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,10 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="manifest" href="/manifest.json">
+    <meta name="theme-color" content="#006400">
+
     @vite('resources/js/app.js')
     @inertiaHead
 </head>
